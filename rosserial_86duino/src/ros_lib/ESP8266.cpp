a575c35fab5bab5731cb2a83ce21511eb1ee5675
CJ Wu <ing5698yi@hotmail.com>
2016-01-18 11:03:13 +0800

    Update ESP8266.cpp

--- rosserial_86duino/src/ros_lib/ESP8266.cpp
+++ rosserial_86duino/src/ros_lib/ESP8266.cpp
@@ -299,6 +299,19 @@ uint32_t ESP8266::recv(uint8_t *coming_mux_id, uint8_t *buffer, uint32_t buffer_
     return recvPkg(buffer, buffer_size, NULL, timeout, coming_mux_id);
 }
 
+bool ESP8266::disableEcho()
+{
+    String data;
+    rx_empty();
+    m_puart->print("ATE0");
+    
+    data = recvString("OK", 100);
+    if (data.indexOf("OK") != -1 ) {
+        return true;
+    }
+    return false;
+}
+
 /*----------------------------------------------------------------------------*/
 /* +IPD,<id>,<len>:<data> */
 /* +IPD,<len>:<data> */
@@ -494,9 +507,9 @@ String ESP8266::recvString(String target, uint32_t timeout)
     while (millis() - start < timeout) {
         while(m_puart->available() > 0) {
             a = m_puart->read();
+			wifi_data.push( a );
 			if(a == '\0') continue;
             data += a;
-			wifi_data.push( a );
         }
         if (data.indexOf(target) != -1) {
             break;
@@ -513,9 +526,9 @@ String ESP8266::recvString(String target1, String target2, uint32_t timeout)
     while (millis() - start < timeout) {
         while(m_puart->available() > 0) {
             a = m_puart->read();
+			wifi_data.push( a );
 			if(a == '\0') continue;
             data += a;
-			wifi_data.push( a );
         }
         if (data.indexOf(target1) != -1) {
             break;
@@ -534,9 +547,9 @@ String ESP8266::recvString(String target1, String target2, String target3, uint3
     while (millis() - start < timeout) {
         while(m_puart->available() > 0) {
             a = m_puart->read();
+			wifi_data.push( a );
 			if(a == '\0') continue;
             data += a;
-			wifi_data.push( a );
         }
         if (data.indexOf(target1) != -1) {
             break;
